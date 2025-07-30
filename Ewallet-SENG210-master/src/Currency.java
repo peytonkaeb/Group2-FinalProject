d22142dc5b086daa4a7243ca95a6a63f3c8f2cbd
gorbyag <artoonist03@gmail.com>
2025-07-29 20:25:42 -0400

    Update currency to support its use in expenseCalculator

--- Ewallet-SENG210-master/src/Currency.java
+++ Ewallet-SENG210-master/src/Currency.java
@@ -2,7 +2,14 @@
 public class Currency {
 	public double rate;
 	public String name;
+	public double convertedAmount;
 	
+	public Currency(String name, double convertedAmount, double exchangeRate){
+		this.name = name;
+		this.rate = exchangeRate;
+		this.convertedAmount = convertedAmount;		
+	}
+
 	public double getRate() {
 		return rate;
 	}
